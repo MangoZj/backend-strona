13715e8b35aeff4d7a080d62a7f8b2d8e23d11e2
MangoZ <Mangomanhw@gmail.com>
2025-01-04 00:51:30 +0100

    Poprawa API

--- api/api.js
+++ api/api.js
@@ -1,7 +1,6 @@
-
 export default function handler(req, res) {
-    res.status(200).json({ message: "API działa poprawnie!" });
-  }
-  
+  res.status(200).json({ message: "API działa poprawnie!" });
+}
+
   
   
